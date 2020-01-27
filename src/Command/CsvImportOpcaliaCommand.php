93b20c9c129a11dc211e51914b8bda19757257f2
ThomasH <thomasheuveline61@gmail.com>
2020-01-27 09:46:14 +0100

    [Core]import csv opcalia

--- src/Command/CsvImportOpcaliaCommand.php
+++ src/Command/CsvImportOpcaliaCommand.php
@@ -6,6 +6,8 @@ namespace App\Command;
 use League\Csv\Reader;
 use App\Entity\Company;
 use App\Entity\Trainee;
+use PhpOffice\PhpSpreadsheet\Cell\Cell;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputOption;
@@ -13,6 +15,7 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use PhpOffice\PhpSpreadsheet\Cell\AdvancedValueBinder;
 
 class CsvImportOpcaliaCommand extends Command
 {
@@ -38,42 +41,67 @@ class CsvImportOpcaliaCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        Cell::setValueBinder(new AdvancedValueBinder());
+        
         $io = new SymfonyStyle($input, $output);
         $io->title('Attempting to import the feed') ;
 
-        $reader = Reader::createFromPath('%kernel.root_dir%/../public/data_opcalia.csv');
+    
+        $inputFileType = 'Csv';
+        $inputFileName = './public/data_opcalia_1.csv';
 
-        $results = $reader->fetchAssoc();
-
-
-        foreach ($results as $row) {
-
-            $trainee = (new Trainee())
-                ->setLastName($row['Prénom'])
-                ->setFirstName($row['NOM'])
-                ->setEmail($row['Email'])          
-            ;
-
-
-            $this->em->persist($trainee);
-
-            $company = (new Company())
-                ->setCorporateName($row['Raison sociale'])
-                ->setStreet($row['Adresse 1'])
-                ->setPostalCode($row['Code Postal'])
-                ->setCity($row['Ville'])
-                ->setSiretNumber($row['Siret'])
-                ->setPhoneNumber($row['Téléphone'])
-            ;
-
-            $this->em->persist($company);
-
-            $trainee->setCompany($company);
-
-        }
+        /**  Create a new Reader of the type defined in $inputFileType  **/
+        $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader($inputFileType);
+        /**  Set the delimiter to a TAB character  **/
+        $reader->setDelimiter(";");
+        $spreadsheet = $reader->load($inputFileName);
+      
+        $loadedSheetNames = $spreadsheet->getSheetNames();
         
-        $this->em->flush();
+        /**  Load the file to a Spreadsheet Object  **/
 
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $output->writeln($loadedSheetNames);
+        
+        $i = 1;
+        // $helper->log($spreadsheet->getSheetCount() . ' worksheet' . (($spreadsheet->getSheetCount() == 1) ? '' : 's') . ' loaded');
+
+        foreach ($loadedSheetNames as $sheetIndex => $loadedSheetName) {
+            // $helper->log('<b>Worksheet #' . $sheetIndex . ' -> ' . $loadedSheetName . ' (Formatted)</b>');
+            $spreadsheet->setActiveSheetIndexByName($loadedSheetName);            
+            $sheetData = $spreadsheet->getActiveSheet()->toArray();
+            for ($i = 1; $i<= sizeof($sheetData)-1; $i++)
+            {
+                $trainee = (new Trainee())
+                    ->setLastName($sheetData[$i][2])
+                    ->setFirstName($sheetData[$i][1])
+                    ->setEmail($sheetData[$i][5])          
+                ;
+
+                $this->em->persist($trainee);
+
+                $company = (new Company())
+                    ->setCorporateName($sheetData[$i][7])
+                    ->setStreet($sheetData[$i][9])
+                    ->setPostalCode($sheetData[$i][11])
+                    ->setCity($sheetData[$i][12])
+                    ->setSiretNumber($sheetData[$i][8])
+                    ->setPhoneNumber($sheetData[$i][4])
+                
+                ;
+
+                $this->em->persist($company);
+            
+                $trainee->setCompany($company);
+
+            }
+
+            
+
+            $this->em->flush();
+            
+            // var_dump($sheetData);
+        }
+    
     }
+    
 }
