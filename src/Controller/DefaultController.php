a825de0802484d6fd5c4915f706184948bb856de
ThomasH <thomasheuveline61@gmail.com>
2020-01-30 17:17:21 +0100

    [Core]Template tally sheet

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Upload;
 use App\Form\UploadType;
 use Psr\Log\LoggerInterface;
+use PhpOffice\PhpWord\PhpWord;
+use PhpOffice\PhpWord\IOFactory;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -47,5 +49,56 @@ class DefaultController extends AbstractController
             'controller_name' => 'DefaultController',
             'form' => $form->createView(),
             ]);
+    }
+
+    /**
+     * @Route ("/test", name="default_test")
+     */
+    public function tallySheetExcel()
+    {
+        echo date('H:i:s'), ' Create new PhpWord object';
+        $phpWord = new \PhpOffice\PhpWord\PhpWord();
+        $section = $phpWord->addSection();
+        $header = ['size' => 30, 'bold' => true];
+
+        // 2. Advanced table
+
+        $section->addTextBreak(1);
+        $section->addText(htmlspecialchars("Feuille d'émargement"), $header, ['align' => 'center']);
+
+        $nbTrainee = 8;
+
+        $styleTable = ['borderSize' => 6, 'borderColor' => '000000', 'cellMargin' => 80];
+        $styleFirstRow = ['borderBottomColor' => '0000FF', 'bgColor' => 'cccccc'];
+        $styleCell = ['valign' => 'center'];
+        
+        $section->addText(htmlspecialchars("Formateur :"));
+        $section->addText(htmlspecialchars("Intilulé de la formation :"));
+        $section->addText(htmlspecialchars("Lieu de la formation :"));
+        $section->addText(htmlspecialchars("Session :"));
+        $section->addTextBreak();
+
+        $phpWord->addTableStyle('Fancy Table', $styleTable, $styleFirstRow);
+        $table = $section->addTable('Fancy Table');
+        $table->addRow(90);
+        $table->addCell(4000, $styleCell)->addText(htmlspecialchars('Nom et prénom'), ['bold' => true, 'size' => 12], ['align' => 'center']);
+        $table->addCell(2000, $styleCell)->addText(htmlspecialchars('Date'), ['bold' => true, 'size' => 12], ['align' => 'center']);
+        $table->addCell(6000, $styleCell)->addText(htmlspecialchars('Signature'), ['bold' => true, 'size' => 12], ['align' => 'center']);
+        for ($i = 1; $i <= $nbTrainee; $i++) {
+            $table->addRow();
+            $table->addCell(2000)->addText(htmlspecialchars(" "));
+            $table->addCell(2000)->addText(htmlspecialchars(" "));
+            $table->addCell(2000)->addText(htmlspecialchars(" "));
+        }
+        
+        $objWriter = IOFactory::createWriter($phpWord, 'Word2007');
+        
+        // Path of saved file
+        $filePath = '../public/emargement.docx';
+
+        // Write file into path
+        $objWriter->save($filePath);
+
+        return new Response("File succesfully written at $filePath");
     }
 }
