b394d39bd003f46958a8329fe2f1e7c4b50469b5
Dylan <dylan.hochet50@gmail.com>
2020-02-06 16:11:17 +0100

    [Core] Zip file DL

--- src/DataFixtures/LocationFixtures.php
+++ src/DataFixtures/LocationFixtures.php
@@ -11,12 +11,14 @@ class LocationFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $location = new Location();
+        $location->setName('Notre Dame de la Providence');
         $location->setStreet('9 rue Chanoine Béranger');
         $location->setPostalCode('50300');
         $location->setCity('AVRANCHES');
         $manager->persist($location);
 
         $location = new Location();
+        $location->setName('Lycée Jean-François Millet');
         $location->setStreet('17 rue de la Consitution');
         $location->setPostalCode('50100');
         $location->setCity('CHERBOURG');
