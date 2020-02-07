76bed7b2f3022c184c120f6d83df96fa39cac9fe
ThomasH <thomasheuveline61@gmail.com>
2020-02-07 10:44:53 +0100

    [Core] clear temp ok

--- src/Migrations/Version20200207093613.php
+++ src/Migrations/Version20200207093613.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200207091703 extends AbstractMigration
+final class Version20200207093613 extends AbstractMigration
 {
     public function getDescription() : string
     {
